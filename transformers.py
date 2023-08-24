7cb16a1fb6a32aa0d0c4288f3ec65ec3513973f2
Aleksandar Pantic <al.pantic@protonmail.com>
2023-08-24 19:20:23 +0200

    v2

--- transformers.py
+++ transformers.py
@@ -1,32 +1,47 @@
 import numpy as np
-from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.compose import ColumnTransformer
 from pandas import DataFrame
 
 
-def format_data(data: DataFrame):
-    data_ = data.iloc[::-1]
-    transformer = create_pipeline()
-    transformer.fit(data_)
-    data_ = transformer.transform(data_)
-    return transformer, data_
-
-def create_sequences(Xnd: np.ndarray, steps=60):
-    rows = Xnd.shape[0]
-    X = []
-    y = []
-    for i in range(steps, rows):
-        X.append(Xnd[i - steps:i, :])
-        y.append(Xnd[i, 0])
-
-    X, y = np.array(X), np.array(y)
-    return X, y
-
-
-def create_pipeline() -> ColumnTransformer:
+class Transformer:
     transformer = ColumnTransformer(transformers=[
         ('drop_date', 'drop', [0]),
         ('open_scaler', StandardScaler(), [1])
     ], remainder=StandardScaler())
-    return transformer
+
+    def __init__(self, steps=60):
+        self.steps = steps
+
+    def fit(self, X: DataFrame):
+        X_ = reverse_data(X)
+        self.transformer.fit(X_)
+
+    def transform(self, new_data):
+
+        new_data_ = reverse_data(new_data)
+        return self.transformer.transform(new_data_)
+
+    def sequence(self, data):
+
+        rows = data.shape[0]
+        X = []
+        y = []
+        for i in range(self.steps, rows):
+            X.append(data[i - self.steps:i, :])
+            y.append(data[i, 0])
+
+        X = np.array(X)
+        y = np.array(y)
+        return X, y
+
+    def get_y_scaler(self):
+        return self.transformer.transformers_[1][1]
+
+
+def reverse_data(data):
+    if type(data) == DataFrame:
+        data_ = data.iloc[::-1]
+    else:
+        data_ = data[::-1]
+    return data_
